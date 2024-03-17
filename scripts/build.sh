64b63310927b5385860c2fe074ec49f26d0e5995
AbhijitKalita <abhijitkalita1984@gmail.com>
2024-03-17 20:40:47 +0530

    updated build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,6 @@
 #!bin/bash
 
 sudo apt-get update -y
-sudo apt-get install apache2-bin -y
-sudo apt-get install  mime-support -y
+sleep 10
 sudo apt-get install apache2 -y
 sudo systemctl start apache2
